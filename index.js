9e7396901979bc166218574907eeb2e41d1327c6
Micu <micking3@gmail.com>
2024-08-05 21:39:40 +0200

    trying some things

---
+++ index.js
@@ -0,0 +1,11 @@
+function helloWorld()
+{
+    //let person = document.getElementById("b1")
+    alert("hello!");
+}
+
+function helloName()
+{
+    let person = document.getElementById("input1").value;    
+    alert("hello! " + person);
+}
